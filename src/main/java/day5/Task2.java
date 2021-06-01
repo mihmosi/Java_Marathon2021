4d3f711fe38ce38d6dd73987ea90963d8d06e7cd
serhii maltsev <semaltsev@luxoft.com>
2021-06-01 09:34:05 +0200

    day 5 tasks 1 and 2

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,8 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motobike bike = new Motobike(1999,"Red","Honda V4");
+        System.out.println("I have  " + bike.getColor() + " " +
+                bike.getModel() + " " +bike.getYearOfIssue() + " year of issue");
     }
 }
