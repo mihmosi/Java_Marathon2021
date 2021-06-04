9902cb71c1a1eb052746d978b8b4e4fa06c6af1b
serhii maltsev <semaltsev@luxoft.com>
2021-06-04 09:46:43 +0200

    tasks for day 8

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,72 @@
+package day8;
+
+public class Airplane {
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public Airplane(String producer, int year, int length, int weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public void fillUp(int n) {
+        fuel += n;
+    }
+
+    public void info() {
+        System.out.println("This is Airplane which producer is " + producer +
+                ", Year of issue is " + year +
+                ", Weight is " + weight +
+                ", Length is " + length +
+                "  and the Value of fuel is " + fuel);
+    }
+
+    @Override
+    public String toString() {
+        return "This is Airplane which producer is " + producer +
+                ", Year of issue is " + year +
+                ", Weight is " + weight +
+                ", Length is " + length +
+                "  and the Value of fuel is " + fuel;
+    }
+
+    public static void compareAirplanes(Airplane firstPlane, Airplane secondPlane) {
+        if((firstPlane.length - secondPlane.length)>0) {
+            System.out.println("first plane is longer");
+        }else if((firstPlane.length - secondPlane.length)==0){
+            System.out.println("the plans have the same length");
+        }else{
+            System.out.println("second plane is longer");
+        }
+    }
+}
